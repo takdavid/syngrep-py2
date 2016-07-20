982661bf43f7c8a6c42e638ea251547f165aed4e
David Takacs <david.takacs@maven7.com>
2016-07-20 19:35:00 +0200

    More unit tests.

--- tests/test_syngrep.py
+++ tests/test_syngrep.py
@@ -1,10 +1,20 @@
 import unittest
-from syngrep import tokenize, is_word
+
+import nltk
+from nltk.corpus.reader import Synset
+from syngrep import tokenize, is_word, pivotize
 
 __all__ = ['TestSyngrep']
 
 
 class TestSyngrep(unittest.TestCase):
+
+    def test_pivotize(self):
+        pivot = pivotize('kid.v.02')
+        self.assertEqual(pivot[0], 'kid.v.02')
+        self.assertEqual(pivot[1], [nltk.wordnet.wordnet.synset('kid.v.02')])
+        self.assertEqual(pivot[2], set([u'banter', u'chaff', u'josh', u'jolly', u'kid']))
+        self.assertEqual(pivot[3], set(['v']))
 
     def test_tokenize(self):
         self.assertEqual(['It', 'was', 'a', 'bright', 'cold', 'day', 'in', 'April', 'and', 'the', 'clocks', 'were', 'striking', 'thirteen'],
